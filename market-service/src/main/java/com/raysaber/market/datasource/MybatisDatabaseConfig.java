a26651ff510305546b4aff304907cf28df19e896
sdboy <jiaoyiguanggo@gmail.com>
2021-04-19 16:42:03 +0800

    解决依赖

--- market-service/src/main/java/com/raysaber/market/datasource/MybatisDatabaseConfig.java
+++ market-service/src/main/java/com/raysaber/market/datasource/MybatisDatabaseConfig.java
@@ -4,18 +4,14 @@ import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
 import javax.sql.DataSource;
-import java.util.List;
 
 /**
  * @author jiaoguang
@@ -26,17 +22,14 @@ import java.util.List;
  */
 @org.springframework.context.annotation.Configuration
 @EnableConfigurationProperties(MybatisPropertiesObject.class)
-@MapperScan(basePackages = "com.raysaber.market.dao", sqlSessionFactoryRef = "sqlSessionFactory")
+//@MapperScan(basePackages = "com.raysaber.market.dao", sqlSessionFactoryRef = "sqlSessionFactory")
 public class MybatisDatabaseConfig {
 
   private final MybatisPropertiesObject mybatisPropertiesObject;
   private final ResourceLoader resourceLoader;
-  private final List<ConfigurationCustomizer> configurationCustomizers;
-  public MybatisDatabaseConfig(MybatisPropertiesObject mybatisPropertiesObject, ResourceLoader resourceLoader,
-                               ObjectProvider<List<ConfigurationCustomizer>> configurationCustomizersProvider) {
+  public MybatisDatabaseConfig(MybatisPropertiesObject mybatisPropertiesObject, ResourceLoader resourceLoader) {
     this.mybatisPropertiesObject = mybatisPropertiesObject;
     this.resourceLoader = resourceLoader;
-    this.configurationCustomizers = configurationCustomizersProvider.getIfAvailable();
   }
 
   @Bean(value = "sqlSessionFactory")
@@ -62,11 +55,6 @@ public class MybatisDatabaseConfig {
     Configuration configuration = mybatisPropertiesObject.getConfiguration();
     if (configuration == null && !StringUtils.hasText(mybatisPropertiesObject.getConfigLocation())) {
       configuration = new Configuration();
-    }
-    if (configuration != null && !CollectionUtils.isEmpty(configurationCustomizers)) {
-      for (ConfigurationCustomizer customizer : configurationCustomizers) {
-        customizer.customize(configuration);
-      }
     }
     factory.setConfiguration(configuration);
   }
