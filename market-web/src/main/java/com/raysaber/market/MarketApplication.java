053e1127f59d60da09d8a896483663e051f912c2
sdboy <jiaoyiguanggo@gmail.com>
2021-04-19 18:55:59 +0800

    bb

--- market-web/src/main/java/com/raysaber/market/MarketApplication.java
+++ market-web/src/main/java/com/raysaber/market/MarketApplication.java
@@ -1,9 +1,12 @@
 package com.raysaber.market;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.raysaber.market.db1", "com.raysaber.market.db2"})
 public class MarketApplication {
 
   public static void main(String[] args) {
